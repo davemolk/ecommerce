3f038e152a37671ceed4dd608dac6e753a8a7d63
davemolk <davemolk@gmail.com>
2021-08-16 15:59:53 -0600

    views folder and urls folder and routing

---
+++ backend/base/urls/user_urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from base.views import user_views as views
+
+from rest_framework_simplejwt.views import (TokenObtainPairView)
+
+
+urlpatterns = [
+    path('', views.getUsers, name="users"),
+    path('register/', views.registerUser, name="register"),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/', views.getUserProfile, name="users-profile"),
+]
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,12 +1,12 @@
-# from django.contrib.auth.models import update_last_login
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from rest_framework.response import Response
-from .models import *
-from .serializers import *
 
+from base.serializers import *
+
+from django.contrib.auth.models import User
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -14,7 +14,6 @@ from rest_framework import status
 
 from django.contrib.auth.hashers import make_password
 
-# Create your views here.
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -67,16 +66,5 @@ def getUsers(request):
     return Response(serializer.data)
 
 
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
 
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
 
